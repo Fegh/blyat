79a43cd4db640985f3f1913ce18248edc87c7f00
Fegh <55936249+Fegh@users.noreply.github.com>
2019-09-30 05:13:56 +0100

    sldkjfhsldkjf

---
+++ something.cs
@@ -0,0 +1,14 @@
+namespace Test
+{
+    public class Calculator
+    {
+        public Calculator() { ... }
+        private double _number;
+        public double Number { get { ... } set { ... } }
+        public void Clear() { ... }
+        private void DoClear() { ... }
+        public double Add(double number) { ... }
+        public static double Pi { ... }
+        public static double GetPi() { ... }
+    }
+}
